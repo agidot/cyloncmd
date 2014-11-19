f1e1676e1ed45ba17b2642a99ec0aabd46ad56e3
mentallake <hybrid_theoryth@hotmail.com>
2014-11-19 16:52:24 +0700

    Add cylon highlight mode

--- app/lib/ace/mode/cylon_highlight_rules.js
+++ app/lib/ace/mode/cylon_highlight_rules.js
@@ -6,17 +6,75 @@ var stringEscape =  "\\\\(x[0-9A-Fa-f]{2}|[0-7]{3}|[\\\\abfnrtv'\"]|U[0-9A-Fa-f]
 
 var CylonHighlightRules = function() {
     // Get Gherkin rules
-    this.$rules = new GherkinHighlightRules().getRules();
+    //this.$rules = new GherkinHighlightRules().getRules();
 
-	// need to include constant ints
-    var newRules = {
+    // need to include constant ints
+    this.$rules = {
         start : [{
-            token: 'comment',
-            regex : 'element'
-        }]
+            token: 'constant.numeric',
+            regex: "(?:(?:[1-9]\\d*)|(?:0))"
+        }, {
+            token : "comment",
+            regex : "#.*$"
+        }, {
+            token : "keyword",
+            regex : "Feature:|Background:|Scenario:|Scenario\ Outline:|Examples:|Given|When|Then|And|But|\\*",
+        }, {
+            token : "string",           // multi line """ string start
+            regex : '"{3}',
+            next : "qqstring3"
+        }, {
+            token : "string",           // " string
+            regex : '"',
+            next : "qqstring"
+        }, {
+            token : "comment",
+            regex : "@[A-Za-z0-9]+",
+            next : "start"
+        }, {
+            token : "comment",
+            regex : "<.+>"
+        }, {
+            token : "comment",
+            regex : "\\| ",
+            next : "table-item"
+        }, {
+            token : "comment",
+            regex : "\\|$",
+            next : "start"
+        }],
+        "qqstring3" : [ {
+            token : "constant.language.escape",
+            regex : stringEscape
+        }, {
+            token : "string", // multi line """ string end
+            regex : '"{3}',
+            next : "start"
+        }, {
+            defaultToken : "string"
+        }],
+        "qqstring" : [{
+            token : "constant.language.escape",
+            regex : stringEscape
+        }, {
+            token : "string",
+            regex : "\\\\$",
+            next  : "qqstring"
+        }, {
+            token : "string",
+            regex : '"|$',
+            next  : "start"
+        }, {
+            defaultToken: "string"
+        }],
+        "table-item" : [{
+            token : "string",
+            regex : "[A-Za-z0-9 ]*",
+            next  : "start"
+        }],
     };
 
-    this.addRules(newRules, "new-");
+    //this.addRules(newRules, '');
 }
 
 oop.inherits(CylonHighlightRules, GherkinHighlightRules);
